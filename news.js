38b60c8a650a0a6a1560b7df341114f56dc27d77
kosmodraiv <60289387+kosmodraiv@users.noreply.github.com>
2022-07-30 21:28:55 +0300

    design-studio

--- news.js
+++ news.js
@@ -1,3 +1,32 @@
+$(".select").click(function() {
+    var is_open = $(this).hasClass("open");
+    if (is_open) {
+      $(this).removeClass("open");
+    } else {
+      $(this).addClass("open");
+    }
+  });
+  
+  $(".select li").click(function() {
+  
+    var selected_value = $(this).html();
+    var first_li = $(".select li:first-child").html();
+  
+    $(".select li:first-child").html(selected_value);
+    $(this).html(first_li);
+  
+  });
+  
+  $(document).mouseup(function(event) {
+  
+    var target = event.target;
+    var select = $(".select");
+  
+    if (!select.is(target) && select.has(target).length === 0) {
+      select.removeClass("open");
+    }
+  
+  });
 
 //news
 const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
